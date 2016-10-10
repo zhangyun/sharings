abae30896a69e8c6769f46d5cbff1f2c1f4a1bdf
Zhang <Raynor.Zhang@emc.com>
2016-10-10 21:29:01 +0800

    Init commit skeleton

---
+++ Refactoring - Java/samples/src/main/java/net/zhangyun/sharings/refactoring/Main.java
@@ -0,0 +1,7 @@
+package net.zhangyun.sharings.refactoring;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("======== Refactoring Samples ========");
+    }
+}
